6de7b9fa0f9ca6252fb32dc6597e1523e021ddf8
Monika Grzanek <monski@o2.pl>
2022-01-19 00:48:19 +0100

    Changes in Product.js and Products.js

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import productsData from '../../data/products';
 import Product from '../Product/Product';
-import PropTypes from 'prop-types';
 
 
 const Products = () => {
@@ -9,16 +8,9 @@ const Products = () => {
 
   return (
     <section>
-      {products.map(product => <Product key={product.id} {...products[0]} />)}
-      {products.map(product => <Product key={product.id} {...products[1]} />)}
+      {products.map((product, index) => { return <Product {...products[index]} key={product.id} />})}
     </section>
   );
-};
-
-Products.propTypes = {
-  name: PropTypes.string, 
-  title: PropTypes.string,
-  basePrice: PropTypes.number,
 }
 
 export default Products;
